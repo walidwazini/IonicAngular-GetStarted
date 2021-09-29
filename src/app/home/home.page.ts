b88e73c35486d580f3998f38bc521bc5984af69c
Walid Wazini <wazini25@gmail.com>
2021-09-29 20:39:05 +0800

    ActionSheet must have button paramete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { ToastController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +16,8 @@ export class HomePage {
 
   constructor(
     public toastController: ToastController,
+    public actionSheetController: ActionSheetController,
+
 
     ) {}
 
@@ -46,4 +48,48 @@ export class HomePage {
     }
   }
 
+  //  Action Sheet
+  async showSheetHandler(){
+    const actionSheet = await this.actionSheetController.create({
+      header:'albums',
+      buttons:[{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler:() => {
+          console.log('Delete clicked');
+        }
+      },
+      {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+    })
+    await actionSheet.present()
+
+    const {role} = await actionSheet.onDidDismiss()
+  }
 }
