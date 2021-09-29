7497c3155f07379cb98133ef26a7cc113f532ef4
Walid Wazini <wazini25@gmail.com>
2021-09-29 14:31:32 +0800

    counter features

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,11 +10,25 @@ export class HomePage {
   // New variable name value is Walid
   name = 'Walid'
   age = 25
+  counter = 0
 
   constructor() {}
 
   sayHello(){
     alert('Hello Walid')
+  }
+  addHandler(){
+    let message = 'add'
+    this.counter++
+  }
+  resetHandler(){
+    let message = 'reset'
+    console.log(message)
+    this.counter = 0
+  }
+  minusHandler(){
+    let message = 'minus'
+    this.counter--
   }
 
 }
